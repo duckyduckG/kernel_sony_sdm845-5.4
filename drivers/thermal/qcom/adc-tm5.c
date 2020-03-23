8bffa6241debcb5277c8422811ec884066d67a6b
Jishnu Prakash <jprakash@codeaurora.org>
2020-03-23 17:05:11 +0530

    thermal: adc_tm: Update support for PMIC4 ADC_TM
    Add new compatible field and corresponding device data
    for PMIC4 ADC_TM to distinguish it from PMIC5, which
    has different device data.
    
    Change-Id: I6c9f20963248c763a6df1f996fd25bdfd6749fa3
    Signed-off-by: Jishnu Prakash <jprakash@codeaurora.org>
    Signed-off-by: Asha Magadi Venkateshamurthy <amagad@codeaurora.org>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/thermal/qcom/adc-tm5.c
+++ drivers/thermal/qcom/adc-tm5.c
@@ -1080,5 +1080,13 @@ const struct adc_tm_data data_adc_tm5 = {
 					1, 2, 4, 8, 16, 32, 64, 128},
 };
 
+const struct adc_tm_data data_adc_tm_rev2 = {
+	.ops			= &ops_adc_tm5,
+	.full_scale_code_volt	= 0x4000,
+	.decimation = (unsigned int []) {256, 512, 1024},
+	.hw_settle = (unsigned int []) {0, 100, 200, 300, 400, 500, 600, 700,
+					800, 900, 1, 2, 4, 6, 8, 10},
+};
+
 MODULE_DESCRIPTION("Qualcomm Technologies Inc. ADC_TM5 driver");
 MODULE_LICENSE("GPL v2");
