ddc5eb92efd5c83a3e236399e91f3331f16175e9
duckyduckG <duckyduckg65@gmail.com>
2025-11-07 23:37:33 +0500

    techpack: display: dp: disable fsa4480 callback

--- techpack/display/msm/dp/dp_display.c
+++ techpack/display/msm/dp/dp_display.c
@@ -1512,6 +1512,11 @@ static int dp_display_init_aux_switch(struct dp_display_private *dp)
 	const char *phandle = "qcom,dp-aux-switch";
 	u32 retry;
 
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+	DP_WARN("FSA4480 AUX switch callback disabled For Xiaomi-SM8150\n");
+	return rc;
+#endif
+
 	if (dp->aux_switch_ready)
 		return rc;
 
@@ -1521,7 +1526,9 @@ static int dp_display_init_aux_switch(struct dp_display_private *dp)
 			phandle, 0);
 	if (!dp->aux_switch_node) {
 		DP_WARN("cannot parse %s handle\n", phandle);
+#if !defined(CONFIG_MACH_XIAOMI_SM8150)
 		rc = -ENODEV;
+#endif
 		return rc;
 	}
 	if (strcmp(dp->aux_switch_node->name,
