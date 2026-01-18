a2a7257ad8595fbabfce80b32e43c6af579f40ef
duckyduckG <duckyduckg65@gmail.com>
2026-01-19 03:33:03 +0500

    techpack: video_legacy: Update msm_vidc_g_crop
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- techpack/video_legacy/msm/vidc/msm_v4l2_vidc.c
+++ techpack/video_legacy/msm/vidc/msm_v4l2_vidc.c
@@ -229,15 +229,14 @@ static int msm_v4l2_g_parm(struct file *file, void *fh,
 {
 	return 0;
 }
-/* TODO: FIX
+
 static int msm_v4l2_g_crop(struct file *file, void *fh,
-			struct v4l2_crop *a)
+			struct v4l2_selection *s)
 {
 	struct msm_vidc_inst *vidc_inst = get_vidc_inst(file, fh);
 
-	return msm_vidc_g_crop(vidc_inst, a);
+	return msm_vidc_g_crop(vidc_inst, s);
 }
-*/
 
 static int msm_v4l2_enum_framesizes(struct file *file, void *fh,
 				struct v4l2_frmsizeenum *fsize)
@@ -287,8 +286,7 @@ static const struct v4l2_ioctl_ops msm_v4l2_ioctl_ops = {
 	.vidioc_encoder_cmd = msm_v4l2_encoder_cmd,
 	.vidioc_s_parm = msm_v4l2_s_parm,
 	.vidioc_g_parm = msm_v4l2_g_parm,
-	/* TODO: FIX
-	.vidioc_g_crop = msm_v4l2_g_crop,*/
+	.vidioc_g_selection = msm_v4l2_g_crop,
 	.vidioc_enum_framesizes = msm_v4l2_enum_framesizes,
 	.vidioc_default = msm_v4l2_default,
 };
