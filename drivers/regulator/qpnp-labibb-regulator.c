kholk/kernel

regulator: qpnp-labibb: Hack LAB/IBB regulators for continuous splash
In continuous splash usecase the regulators are left up and
running in normal state.
Due to a hardware/firmware bug, if we send commands to put it
up, a complete reconfiguration will happen due to bad vreg
state when we boot the kernel: for this, raise the use count
to 1 to avoid the issue.

This hack may be temporary.

drivers/regulator/qpnp-labibb-regulator.c
@@ -2851,8 +2851,15 @@ static int qpnp_labibb_regulator_disable(struct qpnp_labibb *labibb)
 			pr_err("Error in entering TTW mode rc = %d\n", rc);
 			return rc;
 		}
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+		if (labibb->lab_vreg.rdev->use_count == 1)
+			labibb->lab_vreg.vreg_enabled = 0;
+		if (labibb->ibb_vreg.rdev->use_count == 1)
+			labibb->ibb_vreg.vreg_enabled = 0;
+#else
 		labibb->lab_vreg.vreg_enabled = 0;
 		labibb->ibb_vreg.vreg_enabled = 0;
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 		return 0;
 	}
 
@@ -2895,8 +2902,15 @@ static int qpnp_labibb_regulator_disable(struct qpnp_labibb *labibb)
 		}
 	}
 
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+	if (labibb->lab_vreg.rdev->use_count == 1)
+		labibb->lab_vreg.vreg_enabled = 0;
+	if (labibb->ibb_vreg.rdev->use_count == 1)
+		labibb->ibb_vreg.vreg_enabled = 0;
+#else
 	labibb->lab_vreg.vreg_enabled = 0;
 	labibb->ibb_vreg.vreg_enabled = 0;
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 
 	return 0;
 }
@@ -2959,8 +2973,12 @@ static int qpnp_lab_regulator_disable(struct regulator_dev *rdev)
 				REG_LAB_ENABLE_CTL, rc);
 			return rc;
 		}
-
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+		if (rdev->use_count == 1)
+			labibb->lab_vreg.vreg_enabled = 0;
+#else
 		labibb->lab_vreg.vreg_enabled = 0;
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 	}
 	return 0;
 }
@@ -3611,6 +3629,9 @@ static int register_qpnp_lab_regulator(struct qpnp_labibb *labibb,
 
 			return rc;
 		}
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+		labibb->lab_vreg.rdev->use_count = 1;
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 	} else {
 		dev_err(labibb->dev, "qpnp lab regulator name missing\n");
 		return -EINVAL;
@@ -4434,7 +4455,12 @@ static int qpnp_ibb_regulator_disable(struct regulator_dev *rdev)
 			return rc;
 		}
 
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+		if (rdev->use_count == 1)
+			labibb->ibb_vreg.vreg_enabled = 0;
+#else
 		labibb->ibb_vreg.vreg_enabled = 0;
+#endif
 	}
 	return 0;
 }
@@ -4778,6 +4804,9 @@ static int register_qpnp_ibb_regulator(struct qpnp_labibb *labibb,
 
 			return rc;
 		}
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+		labibb->ibb_vreg.rdev->use_count = 1;
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 	} else {
 		dev_err(labibb->dev, "qpnp ibb regulator name missing\n");
 		return -EINVAL;

duckyduckG/kernel_sony_sdm845-5.4

Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

drivers/regulator/qpnp-labibb-regulator.c
@@ -2457,8 +2457,15 @@ static int qpnp_labibb_regulator_disable(struct qpnp_labibb *labibb)
 			pr_err("Error in entering TTW mode rc = %d\n", rc);
 			return rc;
 		}
+#ifdef CONFIG_ARCH_SONY_TAMA
+		if (labibb->lab_vreg.rdev->use_count == 1)
+			labibb->lab_vreg.vreg_enabled = 0;
+		if (labibb->ibb_vreg.rdev->use_count == 1)
+			labibb->ibb_vreg.vreg_enabled = 0;
+#else
 		labibb->lab_vreg.vreg_enabled = 0;
 		labibb->ibb_vreg.vreg_enabled = 0;
+#endif /* CONFIG_ARCH_SONY_TAMA */
 		return 0;
 	}
 
@@ -2501,8 +2508,15 @@ static int qpnp_labibb_regulator_disable(struct qpnp_labibb *labibb)
 		}
 	}
 
+#ifdef CONFIG_ARCH_SONY_TAMA
+	if (labibb->lab_vreg.rdev->use_count == 1)
+		labibb->lab_vreg.vreg_enabled = 0;
+	if (labibb->ibb_vreg.rdev->use_count == 1)
+		labibb->ibb_vreg.vreg_enabled = 0;
+#else
 	labibb->lab_vreg.vreg_enabled = 0;
 	labibb->ibb_vreg.vreg_enabled = 0;
+#endif /* CONFIG_ARCH_SONY_TAMA */
 
 	return 0;
 }
@@ -2568,8 +2582,12 @@ static int qpnp_lab_regulator_disable(struct regulator_dev *rdev)
 				REG_LAB_ENABLE_CTL, rc);
 			return rc;
 		}
-
+#ifdef CONFIG_ARCH_SONY_TAMA
+		if (rdev->use_count == 1)
+			labibb->lab_vreg.vreg_enabled = 0;
+#else
 		labibb->lab_vreg.vreg_enabled = 0;
+#endif /* CONFIG_ARCH_SONY_TAMA */
 	}
 	return 0;
 }
@@ -3239,6 +3257,9 @@ static int register_qpnp_lab_regulator(struct qpnp_labibb *labibb,
 
 			return rc;
 		}
+#ifdef CONFIG_ARCH_SONY_TAMA
+		labibb->lab_vreg.rdev->use_count = 1;
+#endif /* CONFIG_ARCH_SONY_TAMA */
 	} else {
 		dev_err(labibb->dev, "qpnp lab regulator name missing\n");
 		return -EINVAL;
@@ -3778,7 +3799,12 @@ static int qpnp_ibb_regulator_disable(struct regulator_dev *rdev)
 			return rc;
 		}
 
+#ifdef CONFIG_ARCH_SONY_TAMA
+		if (rdev->use_count == 1)
+			labibb->ibb_vreg.vreg_enabled = 0;
+#else
 		labibb->ibb_vreg.vreg_enabled = 0;
+#endif /* CONFIG_ARCH_SONY_TAMA */
 	}
 	return 0;
 }
@@ -4160,6 +4186,9 @@ static int register_qpnp_ibb_regulator(struct qpnp_labibb *labibb,
 
 			return rc;
 		}
+#ifdef CONFIG_ARCH_SONY_TAMA
+		labibb->ibb_vreg.rdev->use_count = 1;
+#endif /* CONFIG_ARCH_SONY_TAMA */
 	} else {
 		dev_err(labibb->dev, "qpnp ibb regulator name missing\n");
 		return -EINVAL;