189008737e3b715989dcaf7d4772cefaa9097890
duckyduckG <duckyduckg65@gmail.com>
2026-02-23 15:57:11 +0500

    techpack: video_legacy: Update Video port resolution while reconfigure events
    Fixes broken formats??

--- techpack/video_legacy/msm/vidc/msm_vidc_clocks.c
+++ techpack/video_legacy/msm/vidc/msm_vidc_clocks.c
@@ -87,15 +87,10 @@ int msm_vidc_get_mbs_per_frame(struct msm_vidc_inst *inst)
 {
 	int height, width;
 
-	if (!inst->in_reconfig) {
-		height = max(inst->prop.height[CAPTURE_PORT],
-			inst->prop.height[OUTPUT_PORT]);
-		width = max(inst->prop.width[CAPTURE_PORT],
-			inst->prop.width[OUTPUT_PORT]);
-	} else {
-		height = inst->reconfig_height;
-		width = inst->reconfig_width;
-	}
+	height = max(inst->prop.height[CAPTURE_PORT],
+		inst->prop.height[OUTPUT_PORT]);
+	width = max(inst->prop.width[CAPTURE_PORT],
+		inst->prop.width[OUTPUT_PORT]);
 
 	return NUM_MBS_PER_FRAME(height, width);
 }
